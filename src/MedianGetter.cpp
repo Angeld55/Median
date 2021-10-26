775b442ba3e6cc9938fc61775249d5100360d208
angeld55 <angel_dimitriev@abv.bg>
2021-10-26 22:59:24 +0300

    implemented median data structure

---
+++ src/MedianGetter.cpp
@@ -0,0 +1,52 @@
+#include "MedianGetter.h"
+
+MedianGetter::MedianGetter(std::initializer_list<int> ilist)
+{
+	for (int el : ilist)
+		insert(el);
+}
+
+void MedianGetter::insert(int n)
+{
+	if (left.empty())
+		left.push(n);
+	else
+	{
+		if (n < left.top())
+			left.push(n);
+		else
+			right.push(n);
+
+		handleHeapsSizes();
+	}
+}
+
+/*
+Set of an even number has no distinct middle value 
+and the median is usually defined the left of two middle value.
+
+Other definition: the arithmetic mean of the two middle values.
+
+The current implemented structure gives the median by the first definition,
+but could be easly modified for the second definition.
+*/
+int MedianGetter::getMedian() const
+{
+	if (left.empty() && right.empty())
+		throw std::logic_error("empty collection!");
+	return left.top();
+}
+
+void MedianGetter::handleHeapsSizes()
+{
+	if (left.size() > right.size() + 1)
+	{
+		right.push(left.top());
+		left.pop();
+	}
+	else if (left.size() < right.size())
+	{
+		left.push(right.top());
+		right.pop();
+	}
+}
---
+++ src/MedianGetter.h
@@ -0,0 +1,20 @@
+#include <queue>
+#include <vector>
+#include <iostream>
+#include <functional>  
+#include <initializer_list> 
+
+
+class MedianGetter
+{
+	std::priority_queue<int> left;
+	std::priority_queue<int, std::vector<int>, std::greater<int>> right;
+
+	void handleHeapsSizes();
+public:
+	MedianGetter() = default;
+	MedianGetter(std::initializer_list<int> ilist);
+
+	void insert(int);
+	int getMedian() const; //for a set of even numbers - gives the left of the two middle values.
+};
