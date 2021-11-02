abfa8613a6d594f2670158df9b81dc1645b47551
Angel Dimitriev <achi_d@abv.bg>
2021-11-02 10:20:56 -0400

    Update MedianTest.cpp

--- tests/MedianTest.cpp
+++ tests/MedianTest.cpp
@@ -20,6 +20,12 @@ bool runTest(TestFunction pfn, const char* name)
 	return result;
 }
 
+bool cmp(double a, double b)
+{
+    static const double EPSILON = 0.000001;
+    return fabs(a - b) < EPSILON;
+}
+
 // Entry point for the application.
 int main()
 {
@@ -37,14 +43,14 @@ int main()
 TEST_CASE(medianGetter_array_sortedAscending)
 {
 	MedianGetter m({ 1, 2, 3, 4, 5 });
-	ASSERT(m.getMedian() == 3);
+	ASSERT(cmp(m.getMedian(),3));
 	return true;
 }
 
 TEST_CASE(medianGetter_array_sortedDescending)
 {
 	MedianGetter m({ 5, 4, 3, 2, 1 });
-	ASSERT(m.getMedian() == 3);
+	ASSERT(cmp(m.getMedian(), 3));
 	return true;
 }
 
@@ -52,27 +58,27 @@ TEST_CASE(medianGetter_array_sortedDescending)
 TEST_CASE(medianGetter_array_sameNumbers)
 {
 	MedianGetter m({1,1,1,1,1,1,1,1,1,1});
-	ASSERT(m.getMedian() == 1);
+	ASSERT(cmp(m.getMedian(), 1));
 	return true;
 }
 
 TEST_CASE(medianGetter_array_numbers)
 {
 	MedianGetter m({ 1, 2, 1, 0, 3, 6, 9, 5, 7 });
-	ASSERT(m.getMedian() == 3);
+	ASSERT(cmp(m.getMedian(), 3));
 	return true;
 }
 
 TEST_CASE(medianGetter_stream_sameNumbers)
 {
 	MedianGetter m({ 1 });
-	ASSERT(m.getMedian() == 1);
+	ASSERT(cmp(m.getMedian(), 1));
 
 	m.insert(1);
-	ASSERT(m.getMedian() == 1);
+	ASSERT(cmp(m.getMedian(), 1));
 
 	m.insert(1);
-	ASSERT(m.getMedian() == 1);
+	ASSERT(cmp(m.getMedian(), 1));
 	return true;
 
 }
@@ -80,19 +86,19 @@ TEST_CASE(medianGetter_stream_sameNumbers)
 TEST_CASE(medianGetter_stream_aroundNumber)
 {
 	MedianGetter m({ 1 });
-	ASSERT(m.getMedian() == 1);
+	ASSERT(cmp(m.getMedian(), 1));
 
 	m.insert(2);
-	ASSERT(m.getMedian() == 1);
+	ASSERT(cmp(m.getMedian(), 1));
 
 	m.insert(0);
-	ASSERT(m.getMedian() == 1);
+	ASSERT(cmp(m.getMedian(), 1));
 
 	m.insert(-1);
-	ASSERT(m.getMedian() == 0);
+	ASSERT(cmp(m.getMedian(), 0));
 
 	m.insert(3);
-	ASSERT(m.getMedian() == 1);
+	ASSERT(cmp(m.getMedian(), 1));
 	return true;
 }
 
@@ -100,28 +106,28 @@ TEST_CASE(medianGetter_stream_increasing)
 {
 	MedianGetter m;
 	m.insert(1);
-	ASSERT(1 == m.getMedian());
+	ASSERT(cmp(1, m.getMedian()));
 
 	m.insert(2);
-	ASSERT(1 == m.getMedian());
+	ASSERT(cmp(1, m.getMedian()));
 
 	m.insert(3);
-	ASSERT(2 == m.getMedian());
+	ASSERT(cmp(2, m.getMedian()));
 
 	m.insert(4);
-	ASSERT(2 == m.getMedian());
+	ASSERT(cmp(2, m.getMedian()));
 
 	m.insert(5);
-	ASSERT(3 == m.getMedian());
+	ASSERT(cmp(3, m.getMedian()));
 
 	m.insert(6);
-	ASSERT(3 == m.getMedian());
+	ASSERT(cmp(3, m.getMedian()));
 
 	m.insert(7);
-	ASSERT(4 == m.getMedian());
+	ASSERT(cmp(4, m.getMedian()));
 
 	m.insert(8);
-	ASSERT(4 == m.getMedian());
+	ASSERT(cmp(4, m.getMedian()));
 	return true;
 }
 
@@ -129,27 +135,27 @@ TEST_CASE(medianGetter_stream_decreasing)
 {
 	MedianGetter m;
 	m.insert(5);
-	ASSERT(5 == m.getMedian());
+	ASSERT(cmp(5, m.getMedian()));
 
 	m.insert(4);
-	ASSERT(4 == m.getMedian());
+	ASSERT(cmp(4, m.getMedian()));
 
 	m.insert(3);
-	ASSERT(4 == m.getMedian());
+	ASSERT(cmp(4, m.getMedian()));
 
 	m.insert(2);
-	ASSERT(3 == m.getMedian());
+	ASSERT(cmp(3, m.getMedian()));
 
 	m.insert(1);
-	ASSERT(3 == m.getMedian());
+	ASSERT(cmp(3, m.getMedian()));
 
 	m.insert(0);
-	ASSERT(2 == m.getMedian());
+	ASSERT(cmp(2, m.getMedian()));
 
 	m.insert(-1);
-	ASSERT(2 == m.getMedian());
+	ASSERT(cmp(2, m.getMedian()));
 
 	m.insert(-2);
-	ASSERT(1 == m.getMedian());
+	ASSERT(cmp(1, m.getMedian()));
 	return true;
 }
