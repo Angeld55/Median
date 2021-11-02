10e177a9f7df49f6861270a67e7115d752fe1dd0
Angel Dimitriev <achi_d@abv.bg>
2021-11-02 10:36:24 -0400

    Update MedianTest.cpp

--- tests/MedianTest.cpp
+++ tests/MedianTest.cpp
@@ -1,7 +1,7 @@
 #include "../src/MedianGetter.h"
 #include <iostream>
 #include <algorithm>
-
+#include <cmath>
 // All test cases are of this type
 using TestFunction = bool(*)();
 
@@ -85,77 +85,81 @@ TEST_CASE(medianGetter_stream_sameNumbers)
 
 TEST_CASE(medianGetter_stream_aroundNumber)
 {
-	MedianGetter m({ 1 });
-	ASSERT(cmp(m.getMedian(), 1));
+        MedianGetter m({ 1 });
+        ASSERT(cmp(m.getMedian(), 1));
 
-	m.insert(2);
-	ASSERT(cmp(m.getMedian(), 1));
+        m.insert(2);
+        ASSERT(cmp(m.getMedian(), 1.5));
 
-	m.insert(0);
-	ASSERT(cmp(m.getMedian(), 1));
+        m.insert(0);
+        ASSERT(cmp(m.getMedian(), 1));
 
-	m.insert(-1);
-	ASSERT(cmp(m.getMedian(), 0));
+        m.insert(-1);
+        ASSERT(cmp(m.getMedian(), 0.5));
 
-	m.insert(3);
-	ASSERT(cmp(m.getMedian(), 1));
-	return true;
+        m.insert(3);
+        ASSERT(cmp(m.getMedian(), 1));
+        return true;
 }
+
+
 
 TEST_CASE(medianGetter_stream_increasing)
 {
-	MedianGetter m;
-	m.insert(1);
-	ASSERT(cmp(1, m.getMedian()));
+        MedianGetter m;
+        m.insert(1);
+        ASSERT(cmp(1, m.getMedian()));
 
-	m.insert(2);
-	ASSERT(cmp(1, m.getMedian()));
+        m.insert(2);
+        ASSERT(cmp(1.5, m.getMedian()));
 
-	m.insert(3);
-	ASSERT(cmp(2, m.getMedian()));
+        m.insert(3);
+        ASSERT(cmp(2, m.getMedian()));
 
-	m.insert(4);
-	ASSERT(cmp(2, m.getMedian()));
+        m.insert(4);
+        ASSERT(cmp(2.5, m.getMedian()));
 
-	m.insert(5);
-	ASSERT(cmp(3, m.getMedian()));
+        m.insert(5);
+        ASSERT(cmp(3, m.getMedian()));
 
-	m.insert(6);
-	ASSERT(cmp(3, m.getMedian()));
+        m.insert(6);
+        ASSERT(cmp(3.5, m.getMedian()));
 
-	m.insert(7);
-	ASSERT(cmp(4, m.getMedian()));
+        m.insert(7);
+        ASSERT(cmp(4, m.getMedian()));
 
-	m.insert(8);
-	ASSERT(cmp(4, m.getMedian()));
-	return true;
+        m.insert(8);
+        ASSERT(cmp(4.5, m.getMedian()));
+        return true;
 }
+
 
 TEST_CASE(medianGetter_stream_decreasing)
 {
-	MedianGetter m;
-	m.insert(5);
-	ASSERT(cmp(5, m.getMedian()));
+        MedianGetter m;
+        m.insert(5);
+        ASSERT(cmp(5, m.getMedian()));
 
-	m.insert(4);
-	ASSERT(cmp(4, m.getMedian()));
+        m.insert(4);
+        ASSERT(cmp(4.5, m.getMedian()));
 
-	m.insert(3);
-	ASSERT(cmp(4, m.getMedian()));
+        m.insert(3);
+        ASSERT(cmp(4, m.getMedian()));
 
-	m.insert(2);
-	ASSERT(cmp(3, m.getMedian()));
+        m.insert(2);
+        ASSERT(cmp(3.5, m.getMedian()));
 
-	m.insert(1);
-	ASSERT(cmp(3, m.getMedian()));
+        m.insert(1);
+        ASSERT(cmp(3, m.getMedian()));
 
-	m.insert(0);
-	ASSERT(cmp(2, m.getMedian()));
+        m.insert(0);
+        ASSERT(cmp(2.5, m.getMedian()));
 
-	m.insert(-1);
-	ASSERT(cmp(2, m.getMedian()));
+        m.insert(-1);
+        ASSERT(cmp(2, m.getMedian()));
 
-	m.insert(-2);
-	ASSERT(cmp(1, m.getMedian()));
-	return true;
+        m.insert(-2);
+        ASSERT(cmp(2.5, m.getMedian()));
+        return true;
 }
+
